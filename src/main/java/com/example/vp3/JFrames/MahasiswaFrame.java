0260f7b37ac46fdf58440916d4a7c30cf88a41f0
thirthfamous <fhbluebeast356@gmail.com>
2024-07-13 10:34:15 +0700

    add image bulbasaur staticly

--- src/main/java/com/example/vp3/JFrames/MahasiswaFrame.java
+++ src/main/java/com/example/vp3/JFrames/MahasiswaFrame.java
@@ -3,13 +3,18 @@ package com.example.vp3.JFrames;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.imageio.ImageIO;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -19,7 +24,7 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 public class MahasiswaFrame {
-    public MahasiswaFrame() {
+    public MahasiswaFrame() throws IOException {
         Connection conn = null;
 
         JFrame jFrame = new JFrame("Aplikasi Hello World");
@@ -27,12 +32,19 @@ public class MahasiswaFrame {
         tableModel.addColumn("ID");
         tableModel.addColumn("Nama");
 
-        tableModel.addRow(new Object[] { 1, "Bulbasaur" });
-        tableModel.addRow(new Object[] { 2, "Squirtle" });
-        tableModel.addRow(new Object[] { 3, "Charmander" });
+        tableModel.addRow(new Object[] { 1, "bulbasaur" });
+        tableModel.addRow(new Object[] { 2, "squirtle" });
+        tableModel.addRow(new Object[] { 3, "charmander" });
 
         JTable table = new JTable(tableModel);
         JLabel pokemonLabel = new JLabel("");
+
+        String path = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/1.png";
+        System.out.println("Get Image from " + path);
+        URL url = new URL(path);
+        BufferedImage image = ImageIO.read(url);        
+        JLabel pokemonImagelaLabel = new JLabel(new ImageIcon(image));
+
         JScrollPane pane = new JScrollPane(table);
 
         table.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -41,7 +53,8 @@ public class MahasiswaFrame {
                 int row = table.rowAtPoint(evt.getPoint());
                 // int col = table.columnAtPoint(evt.getPoint());
                 if (row >= 0) {
-                    pokemonLabel.setText(table.getValueAt(row, 1).toString());
+                    String pokemonName = table.getValueAt(row, 1).toString();
+                    pokemonLabel.setText(pokemonName);            
                 }   
             }
         });
@@ -51,6 +64,7 @@ public class MahasiswaFrame {
         panel.setOpaque(true);
         panel.add(pane);
         panel.add(pokemonLabel);
+        panel.add(pokemonImagelaLabel);
 
         jFrame.getContentPane().add(BorderLayout.CENTER, panel);
         jFrame.pack();
--- src/main/java/com/example/vp3/Main.java
+++ src/main/java/com/example/vp3/Main.java
@@ -1,9 +1,11 @@
 package com.example.vp3;
 
+import java.io.IOException;
+
 import com.example.vp3.JFrames.MahasiswaFrame;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new MahasiswaFrame();
     }
 }
